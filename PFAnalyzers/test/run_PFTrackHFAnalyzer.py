082876d3a976a2e3f9cec96f55c3f10a980971b6
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2019-07-17 11:09:26 -0500

    First set of PFAnalysis/PFAnalyzers

---
+++ PFAnalyzers/test/run_PFTrackHFAnalyzer.py
@@ -0,0 +1,114 @@
+#------------------------------------------------------------------------------------
+# Imports
+#------------------------------------------------------------------------------------
+import FWCore.ParameterSet.Config as cms
+from Configuration.StandardSequences.Eras import eras
+import FWCore.ParameterSet.VarParsing as VarParsing
+
+#------------------------------------------------------------------------------------
+# Declare the process and input variables
+#------------------------------------------------------------------------------------
+#process = cms.Process('NOISE',eras.Run2_50ns)#for 50ns 13 TeV data
+#process = cms.Process('NOISE',eras.Run2_25ns)#for 25ns 13 TeV data
+options = VarParsing.VarParsing ('analysis')
+process = cms.Process("Trees",eras.Phase2) 
+
+##
+## Setup command line options
+##
+options.register ('skipEvents', 0, VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.int, "no of skipped events")
+
+##
+## Default
+##
+options.maxEvents = -1 # -1 means all events
+#options.skipEvents = 0 # default is 0.
+
+##
+## get and parse the command line arguments
+##
+options.parseArguments()
+print("maxEvents: ", options.maxEvents)
+
+#
+# Dataset e.g.
+# dasgoclient --query 'dataset dataset=/RelValTTbar_13/CMSSW_10_2_0_pre3-*realistic*/GEN-SIM-RECO'                 
+# dasgoclient --query 'file dataset=/RelValTTbar_13/CMSSW_10_2_0_pre3-101X_upgrade2018_realistic_v7-v1/GEN-SIM-RECO'
+#
+# TTbar sample
+#
+# RECO
+options.inputFiles = 'file:../../../29088.0_SinglePiPt25Eta1p7_2p7+SinglePiPt25Eta1p7_2p7_2023D41_GenSimHLBeamSpotFull+DigiFullTrigger_2023D41+RecoFullGlobal_2023D41+HARVESTFullGlobal_2023D41/step3.root'
+options.outputFile = 'hist_PFTrackHFAnalyzer.root'
+#
+#
+#
+print("maxEvents: ", options.maxEvents)
+print("inputFiles: ", options.inputFiles)
+print("outputFile: ", options.outputFile)
+
+#------------------------------------------------------------------------------------
+# Get and parse the command line arguments
+#------------------------------------------------------------------------------------
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxEvents) )
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(options.inputFiles),
+    secondaryFileNames = cms.untracked.vstring(options.secondaryInputFiles),
+    skipEvents = cms.untracked.uint32(options.skipEvents) # default is 0.
+)
+
+process.TFileService = cms.Service("TFileService", 
+                                   fileName = cms.string(options.outputFile)
+)
+
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True),
+    Rethrow = cms.untracked.vstring("ProductNotFound"), # make this exception fatal
+    fileMode  =  cms.untracked.string('NOMERGE') # no ordering needed, but calls endRun/beginRun etc. at file boundaries
+)
+
+#------------------------------------------------------------------------------------
+# import of standard configurations
+#------------------------------------------------------------------------------------
+# import of standard configurations
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.Geometry.GeometryExtended2023D41Reco_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.Validation_cff')
+process.load('DQMOffline.Configuration.DQMOfflineMC_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+
+#------------------------------------------------------------------------------------
+# Set up our analyzer
+#------------------------------------------------------------------------------------
+
+#process.load("PFAnalysis.PFAnalyzers.PFTrackHFAnalyzer_cfi")
+process.pfTrackHFAnalyzer = cms.EDAnalyzer("PFTrackHFAnalyzer")
+
+#------------------------------------------------------------------------------------
+# Specify Global Tag
+#------------------------------------------------------------------------------------
+from Configuration.AlCa.GlobalTag import GlobalTag
+#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic', '')
+
+#------------------------------------------------------------------------------------
+# Sequence definition
+#------------------------------------------------------------------------------------
+process.ana_step = cms.Sequence(
+    process.pfTrackHFAnalyzer
+)
+
+#-----------------------------------------------------------------------------------
+# Path and EndPath definitions
+#-----------------------------------------------------------------------------------
+process.preparation = cms.Path(
+    process.ana_step
+)
