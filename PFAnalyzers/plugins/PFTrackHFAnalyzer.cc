3b83dbb49a123f2c3073a73343b4d48000cb375f
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2021-01-24 07:35:26 -0600

    added duplicate checker.

--- PFAnalyzers/plugins/PFTrackHFAnalyzer.cc
+++ PFAnalyzers/plugins/PFTrackHFAnalyzer.cc
@@ -83,6 +83,8 @@ class PFTrackHFAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources>
       edm::EDGetTokenT<CaloParticleCollection> caloparToken_; 
       edm::EDGetTokenT<reco::VertexCollection> vertexToken_; 
       edm::EDGetTokenT<std::vector<reco::PFCandidate>> pfcandToken_; 
+      edm::EDGetTokenT<std::vector<reco::PFCandidate>> pfcandPFAlgoToken_; 
+      edm::EDGetTokenT<std::vector<reco::PFCandidate>> pfcandPFTICLToken_; 
       edm::EDGetTokenT<std::vector<reco::PFCluster>> pfclusterHFToken_; 
       edm::EDGetTokenT<std::vector<reco::PFRecHit>> pfrechitHFToken_; 
       edm::EDGetTokenT<std::vector<reco::PFRecTrack>> pftrackToken_; 
@@ -111,6 +113,8 @@ PFTrackHFAnalyzer::PFTrackHFAnalyzer(const edm::ParameterSet& iConfig)
   caloparToken_(consumes<CaloParticleCollection>(iConfig.getUntrackedParameter<edm::InputTag>("source_calopars"))),
   vertexToken_(consumes<reco::VertexCollection>(iConfig.getUntrackedParameter<edm::InputTag>("source_vertices"))),
   pfcandToken_(consumes<std::vector<reco::PFCandidate>>(iConfig.getUntrackedParameter<edm::InputTag>("source_pfcands"))),
+  pfcandPFAlgoToken_(consumes<std::vector<reco::PFCandidate>>(iConfig.getUntrackedParameter<edm::InputTag>("source_pfcands_pfalgo"))),
+  pfcandPFTICLToken_(consumes<std::vector<reco::PFCandidate>>(iConfig.getUntrackedParameter<edm::InputTag>("source_pfcands_pfticl"))),
   pfclusterHFToken_(consumes<std::vector<reco::PFCluster>>(iConfig.getUntrackedParameter<edm::InputTag>("source_pfclustersHF"))),
   pfrechitHFToken_(consumes<std::vector<reco::PFRecHit>>(iConfig.getUntrackedParameter<edm::InputTag>("source_pfrechitsHF"))),
   pftrackToken_(consumes<std::vector<reco::PFRecTrack>>(iConfig.getUntrackedParameter<edm::InputTag>("source_pftracks"))),
@@ -147,7 +151,11 @@ PFTrackHFAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
    edm::Handle<reco::GenParticleCollection> genpars; iEvent.getByToken(genparToken_, genpars);
    edm::Handle<CaloParticleCollection> calopars; iEvent.getByToken(caloparToken_, calopars);
    edm::Handle<reco::VertexCollection> vertices; iEvent.getByToken(vertexToken_, vertices);
+
    edm::Handle<std::vector<reco::PFCandidate>> pfcands; iEvent.getByToken(pfcandToken_, pfcands);
+   edm::Handle<std::vector<reco::PFCandidate>> pfcands_pfalgo; iEvent.getByToken(pfcandPFAlgoToken_, pfcands_pfalgo);
+   edm::Handle<std::vector<reco::PFCandidate>> pfcands_pfticl; iEvent.getByToken(pfcandPFTICLToken_, pfcands_pfticl);
+
    edm::Handle<std::vector<reco::PFCluster>> pfclustersHF; iEvent.getByToken(pfclusterHFToken_, pfclustersHF);
    edm::Handle<std::vector<reco::PFRecHit>> pfrechitsHF; iEvent.getByToken(pfrechitHFToken_, pfrechitsHF);
    edm::Handle<std::vector<reco::PFRecTrack>> pftracks; iEvent.getByToken(pftrackToken_, pftracks);
@@ -159,6 +167,8 @@ PFTrackHFAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
      LogPrint("PFTrackHFAnalyzer") << "calopars: "     << calopars->size();
      LogPrint("PFTrackHFAnalyzer") << "vertices: "     << vertices->size();
      LogPrint("PFTrackHFAnalyzer") << "pfcands: "      << pfcands->size();
+     LogPrint("PFTrackHFAnalyzer") << "pfcands_pfalgo: "      << pfcands_pfalgo->size();
+     LogPrint("PFTrackHFAnalyzer") << "pfcands_pfticl: "      << pfcands_pfticl->size();
      LogPrint("PFTrackHFAnalyzer") << "pfclustersHF: " << pfclustersHF->size();
      LogPrint("PFTrackHFAnalyzer") << "pfrechitsHF: "  << pfrechitsHF->size();
      LogPrint("PFTrackHFAnalyzer") << "pftracks: "     << pftracks->size();
@@ -170,11 +180,99 @@ PFTrackHFAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
      LogPrint("PFTrackHFAnalyzer") << "calopar: ";
      for(const auto& calopar : *(calopars.product()) )
        LogPrint("PFTrackHFAnalyzer") << boost::format("calopar (pt,eta,phi): (%6.1f, %6.2f, %6.2f)") % calopar.pt() % calopar.eta() % calopar.phi();
-     
+
+     std::vector<unsigned> vtrackKey;
+     std::map<unsigned, unsigned> duplicate;
      LogPrint("PFTrackHFAnalyzer") << "pfcand: ";
      for(const auto& pfcand : *(pfcands.product()) ){
-       if (pfcand.pt()>1.)
-	 LogPrint("PFTrackHFAnalyzer") << boost::format("pfcand (pt,eta,phi): (%6.1f, %6.2f, %6.2f)") % pfcand.pt() % pfcand.eta() % pfcand.phi();
+       if (fabs(pfcand.charge()) && pfcand.trackRef().isNonnull())
+	 LogPrint("PFTrackHFAnalyzer") << boost::format("pfcand (pt,eta,phi): (%6.1f, %6.2f, %6.2f) %5d %5d") % pfcand.pt() % pfcand.eta() % pfcand.phi() % pfcand.pdgId() % pfcand.trackRef().key();
+       if (pfcand.charge() && pfcand.trackRef().isNonnull()) {  // otherwise PFCandidate throws
+	 const auto trackKey = pfcand.trackRef().key();
+	 vtrackKey.push_back(trackKey);
+       }
+     }
+     std::sort(vtrackKey.begin(), vtrackKey.end());
+
+     auto beg = begin(vtrackKey) + 1;
+     for (;beg != end(vtrackKey); ++beg) {
+       if (*beg == *(beg - 1)) {
+	 duplicate[*beg]++;
+       }
+     }
+
+     for (const auto& i : duplicate){
+       std::cout << i.first << " appear " << i.second+1 << " times" << std::endl;
+       LogWarning("PFTrackHFAnalyzer") << boost::format("duplicates");
+
+       for(const auto& pfcand : *(pfcands.product()) ){
+	 if (pfcand.charge()) {  // otherwise PFCandidate throws
+	   const auto trackKey = pfcand.trackRef().key();
+	   if (i.first==trackKey && pfcand.trackRef().isNonnull())
+	     std::cout << " a " 
+		       << pfcand.trackRef()->pt() << " " 
+		       << pfcand.trackRef()->eta() << " " 
+		       << pfcand.trackRef()->phi() << " " 
+		       << pfcand.trackRef().key() << " " 
+		       << pfcand.trackRef().id() << std::endl;
+	 }
+       }
+       for(const auto& pfcand : *(pfcands_pfticl.product()) ){
+	 if (pfcand.charge()) {  // otherwise PFCandidate throws
+	   const auto trackKey = pfcand.trackRef().key();
+	   if (i.first==trackKey && pfcand.trackRef().isNonnull())
+	     std::cout << " pfticl " 
+		       << pfcand.pt() << " " 
+		       << pfcand.eta() << " " 
+		       << pfcand.phi() << " " 
+		       << pfcand.pdgId() << " " 
+		       << pfcand.trackRef()->pt() << " " 
+		       << pfcand.trackRef()->eta() << " " 
+		       << pfcand.trackRef()->phi() << " " 
+		       << pfcand.trackRef()->charge() << " " 
+		       << pfcand.trackRef().key() << " " 
+		       << pfcand.trackRef().id() << std::endl;
+	 }
+       }
+       for(const auto& pfcand : *(pfcands_pfalgo.product()) ){
+	 if (pfcand.charge()) {  // otherwise PFCandidate throws
+	   const auto trackKey = pfcand.trackRef().key();
+	   if (i.first==trackKey && pfcand.trackRef().isNonnull())
+	     std::cout << " pfalgo " 
+		       << pfcand.pt() << " " 
+		       << pfcand.eta() << " " 
+		       << pfcand.phi() << " " 
+		       << pfcand.pdgId() << " " 
+		       << pfcand.trackRef()->pt() << " " 
+		       << pfcand.trackRef()->eta() << " " 
+		       << pfcand.trackRef()->phi() << " " 
+		       << pfcand.trackRef()->charge() << " " 
+		       << pfcand.trackRef().key() << " " 
+		       << pfcand.trackRef().id() << std::endl;
+	 }
+       }
+     }
+
+     vtrackKey.clear();
+     LogPrint("PFTrackHFAnalyzer") << "pfticl: ";
+     for(const auto& pfcand : *(pfcands_pfticl.product()) ){
+       if (fabs(pfcand.charge()))
+	 LogPrint("PFTrackHFAnalyzer") << boost::format("pfcand (pt,eta,phi): (%6.1f, %6.2f, %6.2f) %5d %5d") % pfcand.pt() % pfcand.eta() % pfcand.phi() % pfcand.pdgId() % pfcand.trackRef().key();
+       if (pfcand.charge()) {  // otherwise PFCandidate throws
+	 const auto trackKey = pfcand.trackRef().key();
+	 vtrackKey.push_back(trackKey);
+       }
+     }
+
+     vtrackKey.clear();
+     LogPrint("PFTrackHFAnalyzer") << "pfalgo: ";
+     for(const auto& pfcand : *(pfcands_pfalgo.product()) ){
+       if (fabs(pfcand.charge()))
+	 LogPrint("PFTrackHFAnalyzer") << boost::format("pfcand (pt,eta,phi): (%6.1f, %6.2f, %6.2f) %5d %5d") % pfcand.pt() % pfcand.eta() % pfcand.phi() % pfcand.pdgId() % pfcand.trackRef().key();
+       if (pfcand.charge()) {  // otherwise PFCandidate throws
+	 const auto trackKey = pfcand.trackRef().key();
+	 vtrackKey.push_back(trackKey);
+       }
      }
    
      LogPrint("PFTrackHFAnalyzer") << "pfclusterHF: ";
